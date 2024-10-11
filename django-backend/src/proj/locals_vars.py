aee97219b6460ccbe38a5262aa62748ccd28f430
amadeo <risefenixpuryfire@gmail.com>
2024-10-11 21:38:45 +0300

    ff

---
+++ django-backend/src/proj/locals_vars.py
@@ -0,0 +1,5 @@
+SECRET_KEY = 'django-insecure-wa#75l@ub0+vr1_q^(34nvew(-6$v&lk^vhgbxj5#1z7+q+%65'
+PG_NAME = 'deploy-db' # as a POSTGRES_DB in .pg-env
+PG_USER = 'deploy-test-user' # as in  POSTGRES_USER in .pg-env
+PG_PASSWORD = 'your-deploy-test-password' # as a POSTGRES_PASSWORD  in .pg-env
+PG_HOST = 'postgresql-db' # as the DB's service name in docker-compose.yml
